eb03d30ce2a20f7ff1610bc9a6814073a457f0eb
beata <beata@emc2wave.com>
2020-05-15 16:54:04 -0700

    create searchMeal()

--- script.js
+++ script.js
@@ -5,5 +5,28 @@ const search = document.getElementById('search'),
   resultHeadingEl = document.getElementById('result-heading'),
   single_mealEl = document.getElementById('single-meal');
 
+// Search meal and fetch from API
+const searchMeal = (e) => {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = '';
+
+  // Get search term
+  const term = search.value;
+
+  // Check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeadingEl.innerHTML = `<h2>Search result for '${term}'</h2>`;
+      });
+  } else {
+    alert('Please enter a search term');
+  }
+};
+
 // Event Listener
 submit.addEventListener('submit', searchMeal);
