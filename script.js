ba995284395e96ec1e7949b0e673968adc6dd223
beata <beata@emc2wave.com>
2020-05-15 15:24:16 -0700

    add event listenwer to submit button

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const search = document.getElementById('search'),
   mealsEl = document.getElementById('meals'),
   resultHeadingEl = document.getElementById('result-heading'),
   single_mealEl = document.getElementById('single-meal');
+
+// Event Listener
+submit.addEventListener('submit', searchMeal);
