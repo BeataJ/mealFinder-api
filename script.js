1e02ce6825a037f2d7f1ac3f07797016311692f8
beata <beata@emc2wave.com>
2020-05-16 21:42:10 -0700

    create getRandomMeal()

--- script.js
+++ script.js
@@ -92,9 +92,24 @@ function addMealToDOM(meal) {
   `;
 }
 
+// Fetch random mealfro API
+function getRandomMeal() {
+  // Clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeadingEl.innerHTML = '';
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
 // Event Listener
 submit.addEventListener('submit', searchMeal);
-random.addEventListener('click', randomMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', (e) => {
   const mealInfo = e.path.find((item) => {
