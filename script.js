97cc06789e0c108d0a0dbf03072ba8ada3ef1473
beata <beata@emc2wave.com>
2020-05-16 17:56:50 -0700

    add event lintenet to mealsEl

--- script.js
+++ script.js
@@ -50,3 +50,12 @@ const searchMeal = (e) => {
 
 // Event Listener
 submit.addEventListener('submit', searchMeal);
+mealsEl.addEventListener('click', (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+});
