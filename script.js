355d7b57f8940064d13e8d7dd9a7d4e34eb8ac1a
beata <beata@emc2wave.com>
2020-05-16 19:51:37 -0700

    create getMealById()

--- script.js
+++ script.js
@@ -48,6 +48,17 @@ const searchMeal = (e) => {
   }
 };
 
+// Fetch meal by ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
 // Event Listener
 submit.addEventListener('submit', searchMeal);
 mealsEl.addEventListener('click', (e) => {
